a88fe9cc8d3740129ec1b0f2fdb653c7085e2161
The HungryCoder <zakir@newscred.com>
2012-09-24 11:36:00 +0600

    template tag added

--- newscred/templatetags/newscred_extras.py
+++ newscred/templatetags/newscred_extras.py
@@ -14,19 +14,21 @@ def shorten(url):
   except:
     short_url = url
   return short_url
-@register.tag(name='current_time', do_current_time)
-def do_current_time(parser, token):
+
+class TopicNode(template.Node):
+  def __init__(self, topic):
+    self.topic = template.Variable(topic)
+
+  def render(self, context):
+    topic = self.topic.resolve(context)
+    return ("<div class='info'><br /><hr />Class: %s, Subclass: %s, Group: %s</div>" % (topic['topic_classification'], topic['topic_subclassification'], topic['topic_group'] ) )
+
+
+@register.tag
+def topic_info(parser, token):
   try:
     # split_contents() knows not to split quoted strings.
-    tag_name, format_string = token.split_contents()
+    tag_name, topic = token.split_contents()
   except ValueError:
-    raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-  if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-    raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-  return CurrentTimeNode(format_string[1:-1])
-
-class CurrentTimeNode(template.Node):
-  def __init__(self, format_string):
-    self.format_string = format_string
-  def render(self, context):
-    return datetime.datetime.now().strftime(self.format_string)
+    raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
+  return TopicNode(topic)
